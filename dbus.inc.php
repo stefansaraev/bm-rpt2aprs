62c1e8a2718cad4c6fba5de9803eb3bdbbcc4a1c
Stefan Saraev <stefan@saraev.ca>
2021-12-20 19:21:56 +0200

    dbus improvements

--- dbus.inc.php
+++ dbus.inc.php
@@ -2,21 +2,44 @@
   function dbus_get_repeater_ids_for_network($service) {
     $connection = new DBus(DBus::BUS_SYSTEM, false);
     $proxy = $connection->createProxy($service, OBJECT_PATH, INTERFACE_NAME);
-    $type = new DbusUInt32(2);
-    $result = $proxy->getContextList($type);
+    $result_ids = array();
 
-    if (is_object($result) && get_class($result) == 'DbusArray') {
-      $list = $result->getData();
+    $number = 0;
 
-      foreach ($list as $banner) {
-        $result = $proxy->getRepeaterData($banner);
+    do
+    {
+      $entry = NULL;
 
-        if (is_object($result) && get_class($result) == 'DbusSet') {
-          $set = $result->getData();
-          $result_ids[] = $set[1];
+      $type   = new DbusUInt32(1);
+      $key    = new DbusUInt64($number);
+      $result = $proxy->getCustomList($type, $key);
+
+      if ((is_object($result)) &&
+          (get_class($result) == "DbusArray"))
+      {
+        $list = $result->getData();
+        foreach ($list as $entry)
+        {
+          $value  = $entry->getData();
+          $set    = $value->getData();
+          $number = $set[2];
+
+          $banner = $set[0];
+          $result = $proxy->getRepeaterData($banner);
+
+          if ((is_object($result)) &&
+              (get_class($result) == 'DbusSet'))
+          {
+            $set = $result->getData();
+            $result_ids[] = $set[1];
+          }
         }
       }
+
+      $number ++;
     }
+    while (is_object($entry));
+
     return $result_ids;
   }
 ?>
